0e737147caf87b724475ef6438f8a474958865dd
crayoninvok <dathariqf@gmail.com>
2024-10-26 18:50:25 +0700

    Edited2

--- src/Component/Footer.tsx
+++ src/Component/Footer.tsx
@@ -1,30 +1,33 @@
-import gits from '../assets/github.svg'
-import fb from '../assets/facebook.svg'
-import mail from '../assets/mail.svg'
-import steam from '../assets/steam.png'
+import gits from '../assets/github.svg';
+import fb from '../assets/facebook.svg';
+import mail from '../assets/mail.svg';
+import steam from '../assets/steam.png';
 
-function Footer(){
-    return(
-      <footer className="h-[10vh] justify-center text-center bg-rose-500">
-        <hr className="border-t-2 border-black mb-4" />
-        <div className='pt-[1vh]'>Job Connector Web Development Purwadhika Bandung Property on FrontEnd Projects.</div>
-        <div className="h-[5vh] flex justify-center items-center gap-10">
-          <a href="https://github.com/crayoninvok">
-            <img src={gits} alt="git" />
-          </a>
-          <a href="https://www.facebook.com/profile.php?id=61562493073873&locale=id_ID">
-            <img src={fb} alt="fb" />
-          </a>
-          <a href="mailto:dathariqf@gmail.com">
-            <img src={mail} alt="mail" />
-          </a>
-          <a href="https://steamcommunity.com/profiles/76561198272808740/">
-            <img src={steam} alt="steam" className='w-[31px]' />
-          </a>
-      </div>
-      <div className="h-[2vh] w-full bg-rose-500">@2024 Copyright: Tailwind</div>
-    </footer>
-  );
+function Footer() {
+    return (
+        <footer className="flex flex-col items-center bg-rose-500 py-4 border-t-2 border-black w-full ">
+            <div className='text-sm md:text-base lg:text-lg text-center mb-2'>
+                Job Connector Web Development Purwadhika Bandung Property on FrontEnd Projects.
+            </div>
+            <div className="flex justify-center items-center gap-5 md:gap-10 mb-2">
+                <a href="https://github.com/crayoninvok">
+                    <img src={gits} alt="git" className="w-6 h-6 md:w-8 md:h-8" />
+                </a>
+                <a href="https://www.facebook.com/profile.php?id=61562493073873&locale=id_ID">
+                    <img src={fb} alt="fb" className="w-6 h-6 md:w-8 md:h-8" />
+                </a>
+                <a href="mailto:dathariqf@gmail.com">
+                    <img src={mail} alt="mail" className="w-6 h-6 md:w-8 md:h-8" />
+                </a>
+                <a href="https://steamcommunity.com/profiles/76561198272808740/">
+                    <img src={steam} alt="steam" className="w-6 h-6 md:w-8 md:h-8" />
+                </a>
+            </div>
+            <div className="text-xs md:text-sm lg:text-base w-full bg-rose-500 text-center py-2">
+                @2024 Copyright: Tailwind
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,29 +2,49 @@ import './Style.css';
 import logo from './assets/zap.svg';
 import { Link } from "react-router-dom";
 
-
-function Navbar(){
-    return(
-        <div className="navbar bg-rose-500 px-10 flex justify-center fixed top-0 w-full z-10">
-        <div className='flex-none'><img src={logo} alt="logo" style={{ width: '35px', height: '35px'}}/>
+function Navbar() {
+    return (
+        <div className="navbar bg-rose-500 px-5 md:px-10 flex justify-between items-center fixed top-0 w-full z-10">
+            <div className='flex-none'>
+                <img src={logo} alt="logo" className="w-8 h-8" />
             </div>
-            <div className='flex-1 flex justify-center gap-5 mr-[-100px]'>
-            <li className="btn btn-ghost text-xl" ><Link to="/">Home</Link></li>
-            <li className="btn btn-ghost text-xl"><Link to="About">About</Link></li>
-            <li className="btn btn-ghost text-xl"><Link to="Contact">Contact</Link></li>
+            <div className='hidden md:flex flex-1 justify-center gap-5'>
+                <li className="btn btn-ghost text-lg md:text-xl">
+                    <Link to="/">Home</Link>
+                </li>
+                <li className="btn btn-ghost text-lg md:text-xl">
+                    <Link to="About">About</Link>
+                </li>
+                <li className="btn btn-ghost text-lg md:text-xl">
+                    <Link to="Contact">Contact</Link>
+                </li>
             </div>
+            <label className="swap swap-flip text-3xl md:text-3xl">
+                <input type="checkbox" />
+                <div className="swap-on">🌞</div>
+                <div className="swap-off">🌜</div>
+            </label>
 
-                <label className="swap swap-flip text-9xl">
-                    <input type="checkbox"/>
-                    <div className="swap-on text-4xl">(●'◡'●)</div>
-                    <div className="swap-off text-4xl">╰(*°▽°*)╯</div>
+            {/* Hamburger Menu for Mobile */}
+            <div className="md:hidden flex flex-1 justify-end">
+                <label htmlFor="menu-toggle" className="cursor-pointer text-3xl">
+                    ☰
                 </label>
+                <input type="checkbox" id="menu-toggle" className="hidden" />
+                <div className="absolute right-0 bg-rose-500 p-4 hidden" id="menu-dropdown">
+                    <li className="btn btn-ghost text-lg">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="btn btn-ghost text-lg">
+                        <Link to="About">About</Link>
+                    </li>
+                    <li className="btn btn-ghost text-lg">
+                        <Link to="Contact">Contact</Link>
+                    </li>
+                </div>
+            </div>
         </div>
-    
-    )
+    );
 }
 
-export default Navbar
-
-
-
+export default Navbar;
