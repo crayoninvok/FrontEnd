9d773876d24810d2ab8d2fc0a277faf3734b6484
crayoninvok <dathariqf@gmail.com>
2024-10-27 22:54:39 +0700

    Updated7

--- src/Component/Hero.tsx
+++ src/Component/Hero.tsx
@@ -1,8 +1,12 @@
-
+import React from "react";
 import prx from "../assets/Paper_Rex_darkmode.png";
-import bg2 from '../assets/bg2.mp4'
+import bg2 from '../assets/bg2.mp4';
 
 function Hero() {
+    const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        e.preventDefault(); // Prevent default click behavior
+    };
+
     return (
         <div className="hero min-h-screen relative flex items-center justify-center overflow-hidden">
             <video
@@ -17,9 +21,10 @@ function Hero() {
             <div className="hero-overlay bg-opacity-0 absolute top-0 left-0 w-full h-full z-10"></div>
 
             <div className="hero-content text-neutral-content relative flex flex-col justify-center items-center w-full p-4 mt-20 fade-in">
-            <img src={prx} alt="prx" className="w-full max-w-xs mb-6 w-90"  />
+                <div onClick={handleClick} className="cursor-pointer">
+                    <img src={prx} alt="prx" className="w-full max-w-xs mb-6" />
+                </div>
 
- 
                 <h1 className="mb-7 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold animate-pulse whitespace-nowrap text-center fade-in">
                     console.log(PORTOFOLIO)
                 </h1>
